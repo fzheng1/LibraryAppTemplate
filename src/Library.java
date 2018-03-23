80c4a00d1ccefa586b60b1322e60bad606d51319
BLew1 <36410974+BLew1@users.noreply.github.com>
2018-03-23 09:46:41 -0400

    Update Library.java

--- src/Library.java
+++ src/Library.java
@@ -4,6 +4,13 @@
 public class Library {
 
 
+    String name;
+    Address location;
+    Book[] books;
+    Member[] members;
+    Book book = new Book();
+
+
     /**
      * addBook(Book) : void
      * Creates a new array which is a copy of this.books, but, whose length is
@@ -14,8 +21,18 @@ public class Library {
      * @param book
      *          A new book object to add to the books list
      */
+    public void addBook(Book book) {
+        Book[] books = new Book[(this.books).length + 1];
 
-
+        for (int i = 0; i < books.length; i++){
+            if (i != books.length -1){
+                books[i] = (this.books)[i];
+            } else{
+                books[i].title = book.title;
+            }
+        }
+        this.books = books;
+    }
 
 
 
@@ -30,10 +47,13 @@ public class Library {
      * @param member
      *          A new member object to add to the members list
      */
-
-
-
-
-
-
-}
+    public void addMember(Member member){
+        Member[] members = new Member[(this.members).length + 1];
+        for (int i = 0; i < members.length; i++){
+            if (i != members.length - 1){
+                members[i] = (this.members)[i];
+            } else{
+                members[i].name = member.name;
+            }
+        }
+    }
